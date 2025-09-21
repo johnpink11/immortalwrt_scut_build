96e6a5cd720af1cf4bbfcceab8c167c1dcaabb0d
johnpink11 <73564967+johnpink11@users.noreply.github.com>
2025-09-21 16:05:55 +0800

    Refactor 01_prepare.sh for package management and setup
    Updated feed sources, removed unnecessary packages, and added scutclient and unicom acceleration. Modified startup scripts and ttyd configuration.

--- 01_prepare.sh
+++ 01_prepare.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
+# 添加 feeds 源
 sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
 sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
-rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
 sed -i '20i PKG_USE_MIPS16:=0' feeds/small/dns2tcp/Makefile
 rm -rf feeds/packages/lang/golang
@@ -12,22 +13,29 @@ git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
 rm -rf feeds/packages/net/frp
 git clone https://github.com/kuoruan/openwrt-frp.git package/frp
 
-#单独拉取scutclient
+# 单独拉取 scutclient
 rm -rf feeds/luci/applications/luci-app-scutclient/
 git clone https://github.com/hanwckf/luci-app-scutclient.git feeds/luci/applications/luci-app-scutclient 
 
-#单独拉取联通加速
-mkdir package/scut-unicom
+# 单独拉取联通加速
+mkdir -p package/scut-unicom
 wget https://raw.githubusercontent.com/wykdg/route_script/master/scut-unicom/Makefile -O package/scut-unicom/Makefile
 
 ./scripts/feeds install -a
 
-
-#在启动项加入联通加速
+# 在启动项加入联通加速
 sed -i '3i #如果使用联通加速，删除前面的#，并填充后面的值\n#sleep 10 && /usr/share/scut_unicom/add_route.sh server_ip username password' package/base-files/files/etc/rc.local 
-#将ttyd改成默认root登录
+
+# 将 ttyd 改成默认 root 登录
 sed -i "s/option command '\/bin\/login'/option command '\/bin\/login -f root'/" feeds/packages/utils/ttyd/files/ttyd.config 
 
-
+# 增加 trojan 链接修复
 sed  -i "s/exit 0/[ ! -f '\/usr\/sbin\/trojan' ] \&\& [ -f '\/usr\/bin\/trojan-go' ] \&\& ln -sf \/usr\/bin\/trojan-go \/usr\/bin\/trojan\nexit 0/" package/emortal/default-settings/files/99-default-settings                
 
+### === 新增：选择编译目标 ===
+# 如果需要编译 AX6s (mt7622/RB03)，拷贝对应 seed
+if [ -f "../mt7622_ax6s_config.seed" ]; then
+    echo "[INFO] Using mt7622_ax6s_config.seed for Xiaomi AX6s (RB03)"
+    cp ../mt7622_ax6s_config.seed .config
+    make defconfig
+fi
